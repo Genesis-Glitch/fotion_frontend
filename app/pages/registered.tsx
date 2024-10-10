b6d7857fa91fd7307386e2b72d1f8662aab090ca
goonle <junhuh741@gmail.com>
2024-10-10 16:53:32 +1300

    change registered

--- app/pages/registered.tsx
+++ app/pages/registered.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, ScrollView, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, ScrollView, Text, TouchableOpacity, View, Image} from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 
 import { ThemedView } from '@/components/ThemedView';
@@ -16,8 +16,11 @@ const EventDetails = () => {
   return (
     <ScrollView style={styles.container}>
       <ThemedView style={styles.titleRow}>
-        <Text style={styles.notification}>Successfully Regsitered</Text>
+        <Text style={styles.notification}>Tickets</Text>
       </ThemedView>
+      <View style={styles.qr}>
+          <Image source={{ uri: "/assets/images/Untitled.png" }} style={styles.image} />
+      </View>
       <TouchableOpacity style={styles.button}>
         <Link href="/(tabs)" style={styles.buttonText}>
           Go home
@@ -32,26 +35,45 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingVertical: 30,
     paddingHorizontal: 10,
-    backgroundColor: 'white'
+    backgroundColor: '#F5F5DC', // Neutral Color (Soft Beige)
   },
   titleRow: {
     flexDirection: 'row',
-    justifyContent: 'flex-start',
-    marginBottom: 20,
-    height: 400,
+    marginHorizontal: 'auto',
+    height: 100,
     textAlign: "center",
-    verticalAlign: "middle"
+    alignContent: "center",
+    verticalAlign: "middle",
+    backgroundColor: '#F5F5DC', // Neutral Color (Soft Beige)
+
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+    color: '#4CAF50', // Secondary Color (Fresh Green)
+
   },
+  qr : {
+    marginBottom: 10,
+    alignItems: 'center',
+    borderRadius: 15,
+    // backgroundColor: '#F5F5DC', // Neutral Color (Soft Beige)
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 5,
+    borderRadius: 15,
+    resizeMode: 'cover',
+},
   notification : {
     fontSize: 30 ,
+    color: '#4CAF50', // Secondary Color (Fresh Green)
+
   },
   button: {
     flex: 1,
-    backgroundColor: '#007BFF',
+    backgroundColor: '#FF7043', // Primary Color (Warm Orange)
     padding: 10,
     borderRadius: 10, // Rounding corners
     alignItems: 'center',
